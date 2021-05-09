a09040994e4636be0b44592cbc8a51c05991719f
Nguyen Long <longlcf0311@gmail.com>
2021-05-09 22:14:08 +0700

    upload code

---
+++ src/code/mail/Email.java
@@ -0,0 +1,69 @@
+package code.mail;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+import java.util.Properties;
+
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+
+public class Email {
+	public static void sendEmail(Properties smtpProperties, String toAddress,
+			String subject, String message, File[] attachFiles)
+			throws AddressException, MessagingException, IOException {
+
+		final String userName = smtpProperties.getProperty("mail.user");
+		final String password = smtpProperties.getProperty("mail.password");
+
+		Authenticator auth = new Authenticator() {
+			public PasswordAuthentication getPasswordAuthentication() {
+				return new PasswordAuthentication(userName, password);
+			}
+		};
+		Session session = Session.getInstance(smtpProperties, auth);
+
+		Message msg = new MimeMessage(session);
+
+		msg.setFrom(new InternetAddress(userName));
+		InternetAddress[] toAddresses = { new InternetAddress(toAddress) };
+		msg.setRecipients(Message.RecipientType.TO, toAddresses);
+		msg.setSubject(subject);
+		msg.setSentDate(new Date());
+
+		MimeBodyPart messageBodyPart = new MimeBodyPart();
+		messageBodyPart.setContent(message, "text/html");
+
+		Multipart multipart = new MimeMultipart();
+		multipart.addBodyPart(messageBodyPart);
+
+		if (attachFiles != null && attachFiles.length > 0) {
+			for (File aFile : attachFiles) {
+				MimeBodyPart attachPart = new MimeBodyPart();
+
+				try {
+					attachPart.attachFile(aFile);
+				} catch (IOException ex) {
+					throw ex;
+				}
+
+				multipart.addBodyPart(attachPart);
+			}
+		}
+
+		msg.setContent(multipart);
+
+		Transport.send(msg);
+		
+	}
+}
